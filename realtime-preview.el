2bd21db859975605b5f3b3bf002efbe7b8d0b894
USAMI Kenta <tadsan@zonu.me>
2015-05-16 14:38:20 +0900

    Add copyright and lincense

--- realtime-preview.el
+++ realtime-preview.el
@@ -1,9 +1,33 @@
 ;;; realtime-preview.el --- Realtime preview by eww -*- lexical-binding: t; -*-
 
+;; Copyright (c) 2014 niku
+
 ;; Author: niku <niku@niku.name>
 ;; URL: https://github.com/niku/realtime-preview
 ;; Version 0.0.1
 ;; Package-Requires: ((emacs "24.4"))
+
+
+;; This file is not part of GNU Emacs.
+
+;; The MIT License (MIT)
+
+;; Permission is hereby granted, free of charge, to any person obtaining a copy of
+;; this software and associated documentation files (the "Software"), to deal in
+;; the Software without restriction, including without limitation the rights to
+;; use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+;; the Software, and to permit persons to whom the Software is furnished to do so,
+;; subject to the following conditions:
+;;
+;; The above copyright notice and this permission notice shall be included in all
+;; copies or substantial portions of the Software.
+;;
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+;; FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+;; COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+;; IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+;; CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 ;;; Commentary:
 
